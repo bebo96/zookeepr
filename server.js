4297fcfedf68eb731af7fd978b2fce6a3651aa45
bebo <ibrahimimran96@gmail.com>
2022-09-04 14:52:13 -0400

    Added POST and GET handlers and validators

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -47,6 +54,32 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  }
+  
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -65,6 +98,23 @@ function filterByQuery(query, animalsArray) {
   });
   
 
+//Now when we receive new post data to be added to the animals.json file, we'll take the length property of the animals
+//array (because it's a one-to-one representation of our animals.json file data) and set that as the id for the new data. 
+//Remember, the length property is always going to be one number ahead of the last index of the array so we can avoid any 
+//duplicate values.
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+  });
+  
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
